580d25d9367a7eb9627f10d3c08fec171e9b187d
Caik Franco <caikfra@alu.ufc.br>
2023-04-20 21:21:39 -0300

    Arquivo main.py modificado

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-print("Quantos primos?")
-F = int(input("> "))
-O = F
-N = 2
-i = 1
-Lista = []
-if F <=0:
-    print("Insira um número maior")
-if F > 0:
-    while True:
-        i = i + 1
-        if N % i !=0:
-            i = i + 1
-        if N % i ==0 and i !=N:
-            N = N + 1
-            i = 1
-        if N % i ==0 and i ==N:
-            F = F - 1
-            i = 1
-            Lista.append(N)
-            if F > 0:
-                N = N + 1
-            if F <=0:
-                F = O
-                print((F),Lista)
-                break
+number = int(input('Digite quantos primos quer imprimir: '))
+contador = 0
+i = 2
+while contador < number:
+    teste = 2
+    primo = True
+    while teste < i: 
+        if i % teste ==0 and i !=teste:
+            primo = False
+            break
+        teste = teste +1
+    if primo:
+        contador += 1
+        print(i)
+    i = i + 1
